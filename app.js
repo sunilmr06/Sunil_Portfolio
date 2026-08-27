9cf78b05a88a566d483ba5e77dd8f48e1bc41d26
Sunil M R <smr662003@gmail.com>
2026-08-27 22:00:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
             document.querySelector(".active").classList.remove("active");
             document.getElementById(button.dataset.id).classList.add("active");
         })
-    });
+    }); 
     document.querySelector(".theme-btn").addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
     })
